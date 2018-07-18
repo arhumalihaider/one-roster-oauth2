aac31a6c1c8753ffb2b5ee6161e95c94a3d19683
risteskipetar3@gmail.com <risteskipetar3@gmail.com>
2018-07-18 13:26:00 +0200

    update to OneRoster 1.1

--- lib/oneroster/endpoints.rb
+++ lib/oneroster/endpoints.rb
@@ -1,61 +1,123 @@
 module Oneroster
-  # https://oneroster.infinitecampus.org/campus/oneroster/entropyMaster/learningdata/v1
+  # https://oneroster2.infinitecampus.org/campus/oneroster/entropyMaster/ims/oneroster/v1p1/
+  class AcademicSession
+    get :all, "/academicSessions"
+    get :find, "/academicSessions/:id"
+  end
   class Administrator < Oneroster::ApiBase
     get :all, "/administrators", ignore_root: 'users'
+    get :find, "/administrators/:id", ignore_root: 'users'
+  end
+  class CampusVersion
+    get :version, "/campusVersion"
   end
   class Category < Oneroster::ApiBase
     get :all, "/categories", ignore_root: 'categories'
+    get :find, "/categories/:id", ignore_root: 'categories'
   end
   class Class < Oneroster::ApiBase
     get :all, "/classes", ignore_root: 'classes'
-    get :teachers, "/classes/:id/teachers", ignore_root: 'users'
+    get :find, "/classes/:id", ignore_root: 'classes'
+    get :line_items, "/classes/:id/lineItems"
+    get :line_item_results, "/classes/:id/lineItems/:line_item_id/results"
+    get :results, "/classes/:id/results"
     get :students, "/classes/:id/students", ignore_root: 'users'
+    get :student_results, "/classes/:id/students/:student_id/results"
+    get :teachers, "/classes/:id/teachers", ignore_root: 'users'
   end
   class Course < Oneroster::ApiBase
     get :all, "/courses", ignore_root: 'courses'
+    get :find, "/courses/:id"
+    get :classes, "/courses/:id/classes"
+  end
+  class Demographic
+    get :all, "/demographics"
+    get :find, "/demographics/:id"
   end
   class District < Oneroster::ApiBase
     get :all, "/districts", ignore_root: 'orgs'
   end
   class Enrollment < Oneroster::ApiBase
     get :all, "/enrollments", ignore_root: 'enrollments'
+    get :find, "/enrollments/:id", ignore_root: 'enrollments'
   end
   class GradingPeriod < Oneroster::ApiBase
     get :all, "/gradingPeriods", ignore_root: 'academicSession'
+    get :find, "/gradingPeriods/:id", ignore_root: 'academicSession'
   end
   class Guardian < Oneroster::ApiBase
     get :all, "/guardians", ignore_root: 'users'
+    get :find, "/guardians/:id", ignore_root: 'users'
   end
   class Lineitem < Oneroster::ApiBase
     get :all, "/lineitems", ignore_root: 'lineitems'
+    delete :line_item, "/lineitems/:id"
+    get :find, "/lineitems/:id"
+    put :line_item, "/lineitems/:id"
+    delete :student_result, "/lineitems/:id/students/:student_id/results/:result_id"
+    get :student_result, "/lineitems/:id/students/:student_id/results/:result_id"
+    put :student_result, "/lineitems/:id/students/:student_id/results/:result_id"
+  end
+  class Org
+    get :all, "/orgs"
+    get :find, "/orgs/:id"
+  end
+  class Relative
+    get :all, "/relatives"
+    get :find, "/relatives/:id"
   end
   class Result < Oneroster::ApiBase
     get :all, "/results", ignore_root: 'results'
+    get :find, "/results/:id", ignore_root: 'results'
+    delete :result, "/results/:id", ignore_root: 'results'
+    put :result, "/results/:id", ignore_root: 'results'
   end
   class School < Oneroster::ApiBase
     get :all, "/schools", ignore_root: 'orgs'
     get :find, "/schools/:id", ignore_root: 'org'
+    get :administrators, "/schools/:id/administrators"
     get :classes, "/schools/:id/classes", ignore_root: 'classes'
+    get :class_enrollments, "/schools/:id/classes/:class_id/enrollments"
+    get :class_students, "/schools/:id/classes/:class_id/students"
+    get :class_teachers, "/schools/:id/classes/:class_id/teachers"
+    get :courses, "/schools/:id/courses"
+    get :enrollments, "/schools/:id/enrollments"
+    get :student_enrollments, "/schools/:id/studentEnrollments"
     get :students, "/schools/:id/students", ignore_root: 'users'
+    get :teacher_enrollments, "/schools/:id/teacherEnrollments"
     get :teachers, "/schools/:id/teachers", ignore_root: 'users'
+    get :terms, "/schools/:id/terms"
   end
   class Scoregroup < Oneroster::ApiBase
     get :all, "/scoreGroups", ignore_root: 'scoreGroups'
+    get :find, "/scoreGroups/:id", ignore_root: 'scoreGroups'
+  end
+  class StudentEnrollment
+    get :all, "/studentEnrollments"
   end
   class Student < Oneroster::ApiBase
     get :all, "/students", ignore_root: 'users'
     get :find, "/student/:id", ignore_root: 'users'
+    get :classes, "/student/:id/classes", ignore_root: 'users'
+    get :results, "/student/:id/results", ignore_root: 'users'
+  end
+  class TeacherEnrollment
+    get :all, "/teacherEnrollments"
   end
   class Teacher < Oneroster::ApiBase
     get :all, "/teachers", ignore_root: 'users'
     get :find, "/teacher/:id", ignore_root: 'user'
+    get :classes, "/teacher/:id/classes"
   end
   class Term < Oneroster::ApiBase
     get :all, "/terms", ignore_root: 'academicSessions'
     get :find, "/terms/:id", ignore_root: 'academicSession'
+    get :classes, "/terms/:id/classes"
+    get :grading_periods, "/terms/:id/gradingPeriods"
   end
   class User < Oneroster::ApiBase
     get :all, "/users", ignore_root: 'users'
     get :find, "/user/:id", ignore_root: 'users'
+    get :classes, "/user/:id/classes"
   end
 end
